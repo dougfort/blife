988758dd9a029be474e5196b865234cca7007bb2
dougfort <doug.fort@gmail.com>
2022-08-30 15:51:28 -0400

    split out live_cells creation.
    Initially random map

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use bevy_life::{ConwayCellState, GameOfLife2dPlugin, MooreCell2d, SimulationBatch};
 use rand::Rng;
+use std::collections::HashSet;
 
 fn main() {
     App::new()
@@ -28,10 +29,10 @@ fn setup_map(mut commands: Commands) {
 }
 
 fn spawn_map(commands: &mut Commands) {
-    let mut rng = rand::thread_rng();
     let (size_x, size_y) = (300, 200);
     let sprite_size = 4.;
     let color = Color::rgba(0., 0., 0., 0.);
+    let live_cells = random_map(size_x, size_y);
 
     commands
         .spawn_bundle(SpatialBundle::from_transform(Transform::from_xyz(
@@ -42,7 +43,7 @@ fn spawn_map(commands: &mut Commands) {
         .with_children(|builder| {
             for y in 0..=size_y {
                 for x in 0..=size_x {
-                    let state = ConwayCellState(rng.gen_bool(1. / 3.));
+                    let state = ConwayCellState(live_cells.contains(&(x, y)));
                     builder
                         .spawn_bundle(SpriteBundle {
                             sprite: Sprite {
@@ -63,4 +64,19 @@ fn spawn_map(commands: &mut Commands) {
             }
         });
     println!("map generated");
+}
+
+fn random_map(size_x: i32, size_y: i32) -> HashSet<(i32, i32)> {
+    let mut rng = rand::thread_rng();
+    let mut live_cells: HashSet<(i32, i32)>  = HashSet::new();
+
+    for y in 0..=size_y {
+        for x in 0..=size_x {
+            if rng.gen_bool(1. / 3.) {
+                live_cells.insert((x, y));
+            }
+        }
+    }
+
+    live_cells
 }
