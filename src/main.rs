f8ca95fc903c977abc357fa94c41005fdfe158c0
dougfort <doug.fort@gmail.com>
2022-08-30 14:11:49 -0400

    copy bevy_life example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
+use bevy::prelude::*;
+use bevy_life::{ConwayCellState, GameOfLife2dPlugin, MooreCell2d, SimulationBatch};
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Game Of Life".to_string(),
+            width: 1300.,
+            height: 800.,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .add_plugin(GameOfLife2dPlugin::default())
+        .insert_resource(SimulationBatch::default())
+        .add_startup_system(setup_camera)
+        .add_startup_system(setup_map)
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    // Camera
+    commands.spawn_bundle(Camera2dBundle::default());
+}
+
+fn setup_map(mut commands: Commands) {
+    spawn_map(&mut commands);
+}
+
+fn spawn_map(commands: &mut Commands) {
+    let mut rng = rand::thread_rng();
+    let (size_x, size_y) = (300, 200);
+    let sprite_size = 4.;
+    let color = Color::rgba(0., 0., 0., 0.);
+
+    commands
+        .spawn_bundle(SpatialBundle::from_transform(Transform::from_xyz(
+            -(size_x as f32 * sprite_size) / 2.,
+            -(size_y as f32 * sprite_size) / 2.,
+            0.,
+        )))
+        .with_children(|builder| {
+            for y in 0..=size_y {
+                for x in 0..=size_x {
+                    let state = ConwayCellState(rng.gen_bool(1. / 3.));
+                    builder
+                        .spawn_bundle(SpriteBundle {
+                            sprite: Sprite {
+                                custom_size: Some(Vec2::splat(sprite_size)),
+                                color,
+                                ..Default::default()
+                            },
+                            transform: Transform::from_xyz(
+                                sprite_size * x as f32,
+                                sprite_size * y as f32,
+                                0.,
+                            ),
+                            ..Default::default()
+                        })
+                        .insert(MooreCell2d::new(IVec2::new(x, y)))
+                        .insert(state);
+                }
+            }
+        });
+    println!("map generated");
 }
